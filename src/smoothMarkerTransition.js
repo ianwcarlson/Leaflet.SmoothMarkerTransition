c4c8758b2f8f5ed6aafae1f59b3d8d2c520fbaa7
ianwcarlson <ianwcarlson@gmail.com>
2015-05-22 18:10:10 -0600

    added more documentation

--- src/smoothMarkerTransition.js
+++ src/smoothMarkerTransition.js
@@ -8,10 +8,25 @@ L.SmoothMarkerTransition = L.Marker.extend({
   _holdOffTransition: false,
 
   initialize: function (latlngs, options) {
+    /*
+    Constructor for this class.  Calls base class method with options
+    that can be overridden.
+
+    Args:
+      latlngs: Lat/long object that contains the new coordinates to move to.
+      options: Options that configure this marker class.  The user can apply 
+        the L.Marker options extended option defined in this class.
+    */
     L.Marker.prototype.initialize.call(this, latlngs, options);
   },
 
   transition: function(latlngs) {
+    /*
+    Transitions marker to new location using a CSS transition.
+
+    Args:
+      latlngs: Lat/long object that contains the new coordinates to move to.
+    */
     // Not why I can't do this on initialize()
     this._applyStyleChanges();
     // Move to the next position
@@ -19,10 +34,20 @@ L.SmoothMarkerTransition = L.Marker.extend({
   },
 
   getMarkerID: function(){
+    /*
+    Fetches markerID 
+
+    Returns:
+      A string representing the markerID
+    */
     return this.options.markerID;
   },
 
   disableTransition: function(){
+    /*
+    Disables the transition animation.  Used primarly to work around
+    zooming animations that can be annoying
+    */
     this._holdOffTransition = true;
     if (L.DomUtil.TRANSITION) {
       if (this._icon) { this._icon.style[L.DomUtil.TRANSITION] = ''; }
@@ -31,15 +56,28 @@ L.SmoothMarkerTransition = L.Marker.extend({
   },
 
   enableTransition: function(){
+    /*
+    Re-enables transition animation.  By default, the animation will be 
+    enabled
+    */
     this._holdOffTransition = false;
   },
 
   setTransitionTime: function(transitionTime){
+    /*
+    Set transition time
+
+    Args:
+      transitionTime: A number representing the transition time in milliseconds
+    */
     this.options.traverseTime = transitionTime;
     this._applyStyleChanges();     
   },
 
   _applyStyleChanges: function(){
+    /*
+    Apply style changes to marker object.  Sets the CSS transition attribute
+    */
     if (L.DomUtil.TRANSITION && !this._holdOffTransition) {
       if (this._icon) { this._icon.style[L.DomUtil.TRANSITION] = ('all ' + this.options.traverseTime + 'ms linear'); }
       if (this._shadow) { this._shadow.style[L.DomUtil.TRANSITION] = 'all ' + this.options.traverseTime + 'ms linear'; }
