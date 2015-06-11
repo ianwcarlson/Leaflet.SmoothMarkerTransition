7b6c42be0d7852aa293e580bccda159ebda35098
Ian Carlson <ianwcarlson@Ians-MacBook-Pro-2.local>
2015-06-11 14:07:06 -0600

    fixed marker bug

--- src/smoothMarkerTransition.js
+++ src/smoothMarkerTransition.js
@@ -5,19 +5,33 @@ L.SmoothMarkerTransition = L.Marker.extend({
     markerID: ''
   },
 
+  transitionEnable: true,
+
   initialize: function (latlngs, options) {
     L.Marker.prototype.initialize.call(this, latlngs, options);
   },
 
   transition: function(latlngs) {
 
-    if (L.DomUtil.TRANSITION) {
+    if (L.DomUtil.TRANSITION && this.transitionEnable) {
       if (this._icon) { this._icon.style[L.DomUtil.TRANSITION] = ('all ' + this.options.traverseTime + 'ms linear'); }
       if (this._shadow) { this._shadow.style[L.DomUtil.TRANSITION] = 'all ' + this.options.traverseTime + 'ms linear'; }
     }
 
     // Move to the next position
     this.setLatLng(latlngs);
+  },
+
+  disableTransition: function(){
+    this.transitionEnable = false;
+    if (L.DomUtil.TRANSITION) {
+      if (this._icon) { this._icon.style[L.DomUtil.TRANSITION] = 'all ' + 0 + 'ms linear'; }
+      if (this._shadow) { this._shadow.style[L.DomUtil.TRANSITION] = 'all ' + 0 + 'ms linear'; }
+    }
+  },
+
+  enableTransition: function(){
+    this.transitionEnable = true;
   },
 
   getMarkerID: function(){
